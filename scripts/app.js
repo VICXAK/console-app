bced9b82d2a528bfa027b4673ac61e34123c49de
Victor <hakimov@gmail.com>
2022-05-18 16:32:52 +0500

    Automate questions with a loop

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 /* Create variable numberOfFilms */
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-/* Object personalMovieDB */
+// /* Object personalMovieDB */
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -11,12 +11,23 @@ const personalMovieDB = {
   privat: false,
 };
 
-
-
 /* Questions for user */
-let lastViewedFilm = prompt('Один из последний просмотренных фильмов?', ''),
-      rate = prompt('На сколько оцените его?', '5');
+// let a = prompt('Один из последний просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', '5'),
+//     c = prompt('Один из последний просмотренных фильмов?', ''),
+//     d = prompt('На сколько оцените его?', '5');
 
-/* Add users response */
-personalMovieDB.movies[lastViewedFilm] = rate;
-// console.log(personalMovieDB);
+/* Loop question */
+let lastViewed, rate;
+for (let i = 0; i < 2; i++) {
+  lastViewed = prompt('Один из последний просмотренных фильмов?', ''),
+  rate = prompt('На сколько оцените его?', '5');
+  personalMovieDB.movies[lastViewed] = rate;
+  console.log(lastViewed);
+}
+
+// /* Add users response */
+
+
+console.log(personalMovieDB);
+
