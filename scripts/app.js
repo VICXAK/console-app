c12853019a158f0475695b4788077e4ecaf446a3
Victor <hakimov@gmail.com>
2022-05-18 17:45:44 +0500

    added to check for an empty string and null to the  condition

--- scripts/app.js
+++ scripts/app.js
@@ -18,16 +18,62 @@ const personalMovieDB = {
 //     d = prompt('На сколько оцените его?', '5');
 
 /* Loop question */
-let lastViewed, rate;
-for (let i = 0; i < 2; i++) {
-  lastViewed = prompt('Один из последний просмотренных фильмов?', ''),
-  rate = prompt('На сколько оцените его?', '5');
-  personalMovieDB.movies[lastViewed] = rate;
-  console.log(lastViewed);
-}
 
+for (let i = 0; i < 2; i++) {
+   const lastViewed = prompt('Один из последний просмотренных фильмов?', '');
+   const rate = prompt('На сколько оцените его?', '');
+  if (lastViewed !== '' && rate !== '' && lastViewed !== null && rate !== null && lastViewed.length <= 50) {
+    personalMovieDB.movies[lastViewed] = rate;
+    console.log('done');
+  }
+  else {
+    console.log('error');
+    i--;
+  }
+}
+/*------------------------------- Loop with do {} while()----------------- */
+// let i = 0;
+// do {
+//   const lastViewed = prompt('Один из последний просмотренных фильмов?', '');
+//   const rate = prompt('На сколько оцените его?', '');
+//   if (lastViewed !== '' && rate !== '' && lastViewed !== null && rate !== null && lastViewed.length <= 50) {
+//     personalMovieDB.movies[lastViewed] = rate;
+//     console.log('done');
+//   }
+//   else {
+//     console.log('error');
+//     i--;
+//   }
+//   i++;
+// }while(i < 2);
 // /* Add users response */
 
+/* ---------------------Loop with while(){}---------------------- */
+
+// let i = 0;
+// while (i < 2) {
+//   const lastViewed = prompt('Один из последний просмотренных фильмов?', '');
+//   const rate = prompt('На сколько оцените его?', '');
+//   if (lastViewed !== '' && rate !== '' && lastViewed !== null && rate !== null && lastViewed.length <= 50) {
+//     personalMovieDB.movies[lastViewed] = rate;
+//     console.log('done');
+//   } 
+//   else {
+//     console.log('error');
+//     i--;
+//   }
+//   i++;
+// }
+
+// Condition with answer for users
+if (personalMovieDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert('Вы классический зритель!');
+} else if (personalMovieDB.count >= 30) {
+  alert('Вы киноман!');
+} else {
+  alert('Error');
+}
 
 console.log(personalMovieDB);
-
