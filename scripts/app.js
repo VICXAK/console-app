f3765879dd36337152084c5ac04c32c7acab1490
Victor <hakimov@gmail.com>
2022-05-18 12:48:44 +0500

    create object personalMovieDB

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,12 @@
 /* Create variable numberOfFilms */
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?, '');
+
+/* Object personalMovieDB */
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+}
